c75634cc24d48e5e276e3abde25f8f7f053b7c9e
Ziming Zeng <ziming.zeng@circle.com>
2023-05-14 23:54:49 -0400

    add monthly BTC price overlay specratio_ts

--- code/figure_specratio_ts.py
+++ code/figure_specratio_ts.py
@@ -5,6 +5,7 @@ import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.pyplot import figure
+import matplotlib.dates as mdates
 
 # Create a api_key.py file in the same directory and add your CMC API key
 from api_key import CMC_API_KEY
@@ -25,6 +26,14 @@ def plot_ts_speculative_ratio(
     ts_data["ts_volume"] = ts_data["USDT_volume"] + ts_data["BUSD_volume"]
 
     ts_data = ts_data.merge(usdc_data, left_index=True, right_index=True, how="outer")
+
+    # Fetch daily Bitcoin price data from FRED
+    btc_data = web.DataReader("CBBTCUSD", "fred", start_date, end_date)
+    btc_data.fillna(method="ffill", inplace=True)
+    btc_price_monthly = btc_data["CBBTCUSD"].resample("M").mean()
+    btc_price_monthly.sort_index(inplace=True)
+    btc_price_monthly = btc_price_monthly.reset_index()
+    btc_price_monthly["year"] = btc_price_monthly["DATE"].dt.year
     dtplot = (
         ts_data.resample("Y")
         .mean()
@@ -33,32 +42,82 @@ def plot_ts_speculative_ratio(
             USDC=lambda x: x.USDC_volume / x.USDC_market_cap,
         )
     )
-    dtplot.index = dtplot.index.year
+    # dtplot.index = dtplot.index.year
     combined = dtplot[["USDC", "Trading_Stablecoins"]].rename(
         columns={"Trading_Stablecoins": "Trading\nStablecoins"}
     )
-
-    figure(figsize=(4, 3))
-    ax = plt.axes()
-    ax.set_axisbelow(True)
-    ax.xaxis.grid(False)
-    ax.yaxis.grid(False)
-
-    color_dict = {"USDC": "#2775ca", "Trading Stablecoins": "#c7c5d1"}
-    # combined = combined[["USDC", "Trading Stablecoins"]]
-
-    combined.plot(
-        kind="bar",
-        ax=ax,
-        color=list(color_dict.values()),
-        width=0.75,
-        legend=True,
-        label=["USDC", "Trading/nStablecoins"],
+    combined["year"] = combined.index.year
+    combined_btc = btc_price_monthly.merge(combined, on="year", how="left")
+    print(combined_btc)
+    df_yearly = combined_btc.loc[
+        (combined_btc["DATE"].dt.month == 1) & (combined_btc["DATE"].dt.day == 31)
+    ]
+    df_yearly["DATE"] = pd.to_datetime(df_yearly["DATE"], format="%Y")
+    # Change all dates to 1 for easy to plot purpose
+    # Assuming 'date' is your datetime column in the DataFrame 'df'
+    df_yearly["DATE"] = pd.to_datetime(
+        {
+            "year": df_yearly["DATE"].dt.year,
+            "month": df_yearly["DATE"].dt.month,
+            "day": 1,
+        }
     )
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
+
+    # Start
+    fig, ax1 = plt.subplots(figsize=(4, 3))  #    figure(figsize=(4, 3))
+    ax2 = ax1.twinx()
+    ax2.plot(
+        combined_btc["DATE"],
+        combined_btc["CBBTCUSD"],
+        color="#ADD8E6",
+        label="BTC",
+        linestyle="dashed",
+        linewidth=0.5,
+    )
+    ax2.set_xlabel("Date")
+    # ax2.tick_params("y")
+
+    # We will add/subtract a timedelta to each date for the bars to appear side by side.
+    width = pd.to_timedelta(60, unit="D")  # Change this to adjust the width of bars
+
+    ax1.bar(
+        df_yearly["DATE"] - width,  # subtract width/2 from each date for the first plot
+        df_yearly["USDC"],
+        color="#2775ca",
+        width=2 * width,  # width in terms of days
+        label="USDC",
+        align="center",
+    )
+
+    ax1.bar(
+        df_yearly["DATE"] + width,  # add width/2 to each date for the second plot
+        df_yearly["Trading\nStablecoins"],
+        color="#c7c5d1",
+        width=2 * width,  # width in terms of days
+        label="Trading Stablecoins",
+        align="center",
+    )
+
+    # ax1.tick_params("y")
+
+    # Format x-axis to display years only
+    ax1.xaxis.set_major_locator(mdates.YearLocator())
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%Y"))
+
+    # Add a legend
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
     plt.xticks(rotation=0, fontsize=10)
-    plt.legend(loc="best", fontsize=10, frameon=False)
+    ax1.legend(
+        lines1 + lines2,
+        labels1 + labels2,
+        prop={"size": 8},
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.1),
+        ncol=3,
+    )
+
+    # plt.legend(loc="best", fontsize=10, frameon=False)
 
     plt.savefig("../output/Figure_specratio_ts.pdf", bbox_inches="tight")
 
